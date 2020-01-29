dadbb8ab44130778712759121ad5ed67a580bf46
Susan E. Johnson <seagull62@icloud.com>
2020-01-28 18:10:59 -0800

    added URL & State changes

--- App.js
+++ App.js
@@ -6,11 +6,12 @@ import Nav from './Components/Nav.js';
 import './App.css';
 
 const API = 'https://acme-users-api-rev.herokuapp.com/api';
+const getHash = ()=> window.location.hash.slice(1);
 
 function App() {
   const [ products, setProducts ] = useState([]);
   const [ companies, setCompanies ] = useState([]);
-  const [ view, setView ] = useState('products');
+  const [ view, setView ] = useState(getHash());
 
   useEffect(()=> {
     axios.get(`${API}/companies`)
@@ -20,6 +21,16 @@ function App() {
   useEffect(()=> {
     axios.get(`${API}/products`)
       .then( response => setProducts( response.data ))
+  }, []);
+
+  useEffect(()=> {
+    window.addEventListener('hashchange', ()=> {
+      setView(getHash());
+    });
+
+    if(!['products', 'companies'].includes(getHash())){
+      window.location.hash = 'products';
+    }
   }, []);
 
   return (
